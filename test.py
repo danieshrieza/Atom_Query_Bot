8501c6cb6908961298147866b17f013bc6a7b2a2
danieshrieza <anrieza55@gmail.com>
2021-10-12 15:50:32 +0800

    Testing new feature

--- test.py
+++
@@ -1,9 +0,0 @@
-string = "Hello * B/e"
-
-for i in string :
-    if ("*" or "/") in string :
-        global new_string
-        new_string = string.replace("*", "×").replace("/", "÷")
-
-print(new_string)
-
