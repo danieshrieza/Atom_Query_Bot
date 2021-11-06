30d470b9fa77bdec4bae3fdb0403321d22a0d37e
danieshrieza <anrieza55@gmail.com>
2021-11-06 22:44:48 +0800

    Getting name for all the server that host the Discord Bot

--- components/test_cmd.py
+++ components/test_cmd.py
@@ -8,11 +8,14 @@ class test_cmd(commands.Cog):
 
     @ commands.command(hidden = True)
     @ commands.is_owner()
-    async def hello(self, ctx):
-        
-        if (ctx.guild.id == 883147972337090610) :
-            await ctx.send("hello")
+    async def showguilds(self, ctx):
 
+        if (ctx.guild.id == 883147972337090610) :
+            guild_msg = ""
+            for guild in self.bot.guilds:
+                guild_msg += f"{guild.name}\n"
+            await ctx.send(guild_msg)
+        
         else :
             await ctx.send("This command is not allowed on this server.", delete_after = 1)
             await ctx.channel.purge(limit = 2)
