5711b29202dcdc7ccadd3f7bd5edb92702c70bb4
danieshrieza <anrieza55@gmail.com>
2022-01-19 13:16:32 +0800

    Make guild id into integer

--- config.py
+++ config.py
@@ -5,7 +5,9 @@ from dotenv.main import load_dotenv
 load_dotenv()
 
 # <--- Get the guild id from .env file --->
-guild_id = int(os.getenv("GUILD_VAR", ""))
+guild_var = os.getenv("GUILD_VAR", "")
+
+guild_id = int(guild_var)
 
 # <--- Get the bot token from .env file --->
 token = os.getenv("MATH_VAR", "")
