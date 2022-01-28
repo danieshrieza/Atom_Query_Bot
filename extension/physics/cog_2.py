29cf4bac4a5553e39b7db96d211cff6016257b47
danieshrieza <anrieza55@gmail.com>
2022-01-28 16:18:44 +0800

    Added several new commands in physics folder

--- extension/physics/cog_2.py
+++ extension/physics/cog_2.py
@@ -116,6 +116,174 @@ class EnergyAndForce(commands.Cog) :
         await ctx.response.send_message(embed = embed_msg)
 
 
+    # NOTE : Command to calculate the work done of an object 
+    @ nextcord.slash_command(
+        description = "Calculate the work done of an object."
+    )
+
+    async def work_done(self, ctx : Interaction, force : float, distance : float) :
+
+        exp = f'{force} N × {distance} m'
+        evalu = force * distance
+
+        embed_msg = nextcord.Embed(
+            title = "Physics Query", 
+            description = "The requested `Physics Query` have been evaluated by **Atom Query**", 
+            timestamp = datetime.now(timezone.utc), 
+            colour = nextcord.Color.from_rgb(127, 0, 255)
+        )
+
+        embed_msg.add_field(
+            name = "Input :", 
+            value = f"`{exp}`", 
+            inline = False
+        )
+
+        embed_msg.add_field(
+            name = "Output :", 
+            value = f"`{evalu} J`", 
+            inline = True
+        )
+        
+        embed_msg.set_thumbnail(url = self.link)
+
+        await ctx.response.send_message(embed = embed_msg)
+
+
+    # NOTE : Command to calculate the power possessed by an object 
+    @ nextcord.slash_command(
+        description = "Calculate the power possessed by an object."
+    )
+
+    async def power(self, ctx : Interaction, force : float, distance : float, time : float) :
+
+        exp = f'({force} N × {distance} m) ÷ {time} s'
+        evalu = (force * distance) / time
+
+        embed_msg = nextcord.Embed(
+            title = "Physics Query", 
+            description = "The requested `Physics Query` have been evaluated by **Atom Query**", 
+            timestamp = datetime.now(timezone.utc), 
+            colour = nextcord.Color.from_rgb(127, 0, 255)
+        )
+
+        embed_msg.add_field(
+            name = "Input :", 
+            value = f"`{exp}`", 
+            inline = False
+        )
+
+        embed_msg.add_field(
+            name = "Output :", 
+            value = f"`{evalu} W`", 
+            inline = True
+        )
+        
+        embed_msg.set_thumbnail(url = self.link)
+
+        await ctx.response.send_message(embed = embed_msg)
+
+
+    # NOTE : Command to calculate the gravitational potential energy possessed by an object 
+    @ nextcord.slash_command(
+        description = "Calculate the gravitational potential energy possessed by an object."
+    )
+
+    async def grav_potential_energy(self, ctx : Interaction, mass : float, distance : float) :
+
+        exp = f'{mass} kg × 9.8 m/s × {distance} m'
+        evalu = mass * 9.8 * distance
+
+        embed_msg = nextcord.Embed(
+            title = "Physics Query", 
+            description = "The requested `Physics Query` have been evaluated by **Atom Query**", 
+            timestamp = datetime.now(timezone.utc), 
+            colour = nextcord.Color.from_rgb(127, 0, 255)
+        )
+
+        embed_msg.add_field(
+            name = "Input :", 
+            value = f"`{exp}`", 
+            inline = False
+        )
+
+        embed_msg.add_field(
+            name = "Output :", 
+            value = f"`{evalu} J`", 
+            inline = True
+        )
+        
+        embed_msg.set_thumbnail(url = self.link)
+
+        await ctx.response.send_message(embed = embed_msg)
+
+
+    # NOTE : Command to calculate the elastic potential energy possessed by an object 
+    @ nextcord.slash_command(
+        description = "Calculate the elastic potential energy possessed by an object."
+    )
+
+    async def elas_potential_energy(self, ctx : Interaction, force : float, original_length : float, new_length : float) :
+
+        exp = f'1/2 × {force} N × ({original_length} - {new_length}) m'
+        evalu = 1/2 * (force * (original_length - new_length))
+
+        embed_msg = nextcord.Embed(
+            title = "Physics Query", 
+            description = "The requested `Physics Query` have been evaluated by **Atom Query**", 
+            timestamp = datetime.now(timezone.utc), 
+            colour = nextcord.Color.from_rgb(127, 0, 255)
+        )
+
+        embed_msg.add_field(
+            name = "Input :", 
+            value = f"`{exp}`", 
+            inline = False
+        )
+
+        embed_msg.add_field(
+            name = "Output :", 
+            value = f"`{evalu} J`", 
+            inline = True
+        )
+        
+        embed_msg.set_thumbnail(url = self.link)
+
+
+    # NOTE : Command to calculate the kinetic energy possessed by an object 
+    @ nextcord.slash_command(
+        description = "Calculate the kinetic energy possessed by an object."
+    )
+
+    async def kinetic_energy(self, ctx : Interaction, mass : float, distance : float, time : float) :
+
+        exp = f'1/2 × {mass} kg × ({distance / time} m/s)²'
+        evalu = 1/2 * mass * (distance / time) ** 2
+
+        embed_msg = nextcord.Embed(
+            title = "Physics Query", 
+            description = "The requested `Physics Query` have been evaluated by **Atom Query**", 
+            timestamp = datetime.now(timezone.utc), 
+            colour = nextcord.Color.from_rgb(127, 0, 255)
+        )
+
+        embed_msg.add_field(
+            name = "Input :", 
+            value = f"`{exp}`", 
+            inline = False
+        )
+
+        embed_msg.add_field(
+            name = "Output :", 
+            value = f"`{evalu} J`", 
+            inline = True
+        )
+        
+        embed_msg.set_thumbnail(url = self.link)
+
+        await ctx.response.send_message(embed = embed_msg)
+
+
 # NOTE : Add EnergyAndForce to the bot
 def setup(bot : commands.Bot) :
     bot.add_cog(EnergyAndForce(bot))
