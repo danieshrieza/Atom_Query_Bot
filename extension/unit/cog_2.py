ca2244d1aa56ba7784e39bcf2f02f3c0627724cf
danieshrieza <anrieza55@gmail.com>
2022-01-30 22:24:52 +0800

    Added few new commands in Unit Converter

--- extension/unit/cog_2.py
+++ extension/unit/cog_2.py
@@ -203,6 +203,223 @@ class MassAndVolumeAndFrac(commands.Cog) :
         await ctx.response.send_message(embed = embed_msg)
 
 
+    # NOTE : Command to convert kilogram to Newton
+    @ nextcord.slash_command(
+        description = "Convert kilogram to Newton."
+    )
+
+    async def kg_to_N(self, ctx : Interaction, kilogram : float) :
+
+        embed_msg = nextcord.Embed(
+            title = "Unit Converter", 
+            description = "The requested `Unit Conversion` have been evaluated by **Atom Query**", 
+            timestamp = datetime.now(timezone.utc),
+            colour = nextcord.Color.from_rgb(178, 34, 34)
+        )
+
+        embed_msg.add_field(
+            name = "Input :", 
+            value = f"```Python\n {kilogram} kg \n```", 
+            inline = False
+        )
+
+        embed_msg.add_field(
+            name = "Output :", 
+            value = f"```Python\n {kilogram * 9.8} \n```", 
+            inline = True
+        )
+
+        embed_msg.set_thumbnail(url = self.link)
+        
+        await ctx.response.send_message(embed = embed_msg)
+
+
+    # NOTE : Command to convert Newton to kilogram
+    @ nextcord.slash_command(
+        description = "Convert Newton to kilogram."
+    )
+
+    async def N_to_kg(self, ctx : Interaction, newton : float) :
+
+        embed_msg = nextcord.Embed(
+            title = "Unit Converter", 
+            description = "The requested `Unit Conversion` have been evaluated by **Atom Query**", 
+            timestamp = datetime.now(timezone.utc),
+            colour = nextcord.Color.from_rgb(178, 34, 34)
+        )
+
+        embed_msg.add_field(
+            name = "Input :", 
+            value = f"```Python\n {newton} N \n```", 
+            inline = False
+        )
+
+        embed_msg.add_field(
+            name = "Output :", 
+            value = f"```Python\n {newton / 9.8} \n```", 
+            inline = True
+        )
+
+        embed_msg.set_thumbnail(url = self.link)
+        
+        await ctx.response.send_message(embed = embed_msg)
+
+
+    # NOTE : Command to convert kg to Newton
+    @ nextcord.slash_command(
+        description = "Convert kilogram to Newton."
+    )
+
+    async def kg_to_N(self, ctx : Interaction, kilogram : float) :
+
+        embed_msg = nextcord.Embed(
+            title = "Unit Converter", 
+            description = "The requested `Unit Conversion` have been evaluated by **Atom Query**", 
+            timestamp = datetime.now(timezone.utc),
+            colour = nextcord.Color.from_rgb(178, 34, 34)
+        )
+
+        embed_msg.add_field(
+            name = "Input :", 
+            value = f"```Python\n {kilogram} kg \n```", 
+            inline = False
+        )
+
+        embed_msg.add_field(
+            name = "Output :", 
+            value = f"```Python\n {kilogram * 9.8} \n```", 
+            inline = True
+        )
+
+        embed_msg.set_thumbnail(url = self.link)
+        
+        await ctx.response.send_message(embed = embed_msg)
+
+
+    # NOTE : Command to convert Astronomical Unit to kilometer
+    @ nextcord.slash_command(
+        description = "Convert Astronomical Unit to kilometer."
+    )
+
+    async def AU_to_km(self, ctx : Interaction, AU : float) :
+
+        embed_msg = nextcord.Embed(
+            title = "Unit Converter", 
+            description = "The requested `Unit Conversion` have been evaluated by **Atom Query**", 
+            timestamp = datetime.now(timezone.utc),
+            colour = nextcord.Color.from_rgb(178, 34, 34)
+        )
+
+        embed_msg.add_field(
+            name = "Input :", 
+            value = f"```Python\n {AU} A.U \n```", 
+            inline = False
+        )
+
+        embed_msg.add_field(
+            name = "Output :", 
+            value = f"```Python\n {AU * (1.5 * 10 ** 8)} km \n```", 
+            inline = True
+        )
+
+        embed_msg.set_thumbnail(url = self.link)
+        
+        await ctx.response.send_message(embed = embed_msg)
+
+
+    # NOTE : Command to convert kilometer to Astronomical Unit
+    @ nextcord.slash_command(
+        description = "Convert kilometer to Astronomical Unit."
+    )
+
+    async def km_to_AU(self, ctx : Interaction, km : float) :
+
+        embed_msg = nextcord.Embed(
+            title = "Unit Converter", 
+            description = "The requested `Unit Conversion` have been evaluated by **Atom Query**", 
+            timestamp = datetime.now(timezone.utc),
+            colour = nextcord.Color.from_rgb(178, 34, 34)
+        )
+
+        embed_msg.add_field(
+            name = "Input :", 
+            value = f"```Python\n {km} km \n```", 
+            inline = False
+        )
+
+        embed_msg.add_field(
+            name = "Output :", 
+            value = f"```Python\n {km / (1.5 * 10 ** 8)} A.U \n```", 
+            inline = True
+        )
+
+        embed_msg.set_thumbnail(url = self.link)
+        
+        await ctx.response.send_message(embed = embed_msg)
+
+
+    # NOTE : Command to convert Light Year to kilometer
+    @ nextcord.slash_command(
+        description = "Convert Light Year to kilometer."
+    )
+
+    async def ly_to_km(self, ctx : Interaction, ly : float) :
+
+        embed_msg = nextcord.Embed(
+            title = "Unit Converter", 
+            description = "The requested `Unit Conversion` have been evaluated by **Atom Query**", 
+            timestamp = datetime.now(timezone.utc),
+            colour = nextcord.Color.from_rgb(178, 34, 34)
+        )
+
+        embed_msg.add_field(
+            name = "Input :", 
+            value = f"```Python\n {ly} ly \n```", 
+            inline = False
+        )
+
+        embed_msg.add_field(
+            name = "Output :", 
+            value = f"```Python\n {ly * (9.5 * 10 ** 12)} km \n```", 
+            inline = True
+        )
+
+        embed_msg.set_thumbnail(url = self.link)
+        
+        await ctx.response.send_message(embed = embed_msg)
+
+
+    # NOTE : Command to convert kilometer to Light Year
+    @ nextcord.slash_command(
+        description = "Convert kilometer to Light Year."
+    )
+
+    async def km_to_ly(self, ctx : Interaction, km : float) :
+
+        embed_msg = nextcord.Embed(
+            title = "Unit Converter", 
+            description = "The requested `Unit Conversion` have been evaluated by **Atom Query**", 
+            timestamp = datetime.now(timezone.utc),
+            colour = nextcord.Color.from_rgb(178, 34, 34)
+        )
+
+        embed_msg.add_field(
+            name = "Input :", 
+            value = f"```Python\n {km} km \n```", 
+            inline = False
+        )
+
+        embed_msg.add_field(
+            name = "Output :", 
+            value = f"```Python\n {km / (9.5 * 10 ** 12)} km \n```", 
+            inline = True
+        )
+
+        embed_msg.set_thumbnail(url = self.link)
+        
+        await ctx.response.send_message(embed = embed_msg)
+
+
 # NOTE : Add MassAndVolumeAndFrac to the bot
 def setup(bot : commands.Bot) :
     bot.add_cog(MassAndVolumeAndFrac(bot))
