5ce66b259b2c79f45547bd599f6bb1bef61ed33c
danieshrieza <anrieza55@gmail.com>
2022-03-13 22:06:37 +0800

    Wrap the code in bot.py with main function

--- bot.py
+++ bot.py
@@ -5,42 +5,51 @@ from nextcord.ext.commands.context import Context
 from config import TOKEN, OWNER_ID
 
 
-# NOTE : Declare bot variable
-bot = commands.Bot(
-    command_prefix="!",
-    case_insensitive=True,
-    strip_after_prefix=True,
-    help_command=None,
-    owner_id=OWNER_ID,
-    intents=nextcord.Intents.all(),
-    activity=nextcord.Activity(type=nextcord.ActivityType.watching, name="The Complex Plane")
-)
+# NOTE : Declare main function which houses the code for the bot
+def main():
+    
+    
+    # NOTE : Declare bot variable
+    bot = commands.Bot(
+        command_prefix="!",
+        case_insensitive=True,
+        strip_after_prefix=True,
+        help_command=None,
+        owner_id=OWNER_ID,
+        intents=nextcord.Intents.all(),
+        activity=nextcord.Activity(type=nextcord.ActivityType.watching, name="The Complex Plane")
+    )
 
 
-# # NOTE : Declare tree variable for slash command
-# tree = app_commands.CommandTree(bot)
+    # # NOTE : Declare tree variable for slash command
+    # tree = app_commands.CommandTree(bot)
 
 
-# NOTE : Activating bot once it's ready
-@ bot.event
-async def on_ready():
-    # await tree.sync()
-    print(f"I have logged in as {bot.user}")
+    # NOTE : Activating bot once it's ready
+    @ bot.event
+    async def on_ready():
+        # await tree.sync()
+        print(f"I have logged in as {bot.user}")
 
 
-# NOTE : Command to find all server that houses this bot
-@ commands.is_owner()
-@ bot.command()
-async def server(ctx: Context):
-    for guild in bot.guilds:
-        await ctx.send(f"{guild.name}")
+    # NOTE : Command to find all server that houses this bot
+    @ commands.is_owner()
+    @ bot.command()
+    async def server(ctx: Context):
+        for guild in bot.guilds:
+            await ctx.send(f"{guild.name}")
 
 
-# NOTE : Load extension file once bot is ready
-for file in os.listdir("./extension"):
-    if file.endswith(".py") :
-        bot.load_extension(f"extension.{file[:-3]}")
+    # NOTE : Load extension file once bot is ready
+    for file in os.listdir("./extension"):
+        if file.endswith(".py") :
+            bot.load_extension(f"extension.{file[:-3]}")
 
 
-# NOTE : Key for bot to run
-bot.run(TOKEN)
+    # NOTE : Key for bot to run
+    bot.run(TOKEN)
+
+
+# NOTE : Call main
+if __name__ == "__main__":
+    main()
